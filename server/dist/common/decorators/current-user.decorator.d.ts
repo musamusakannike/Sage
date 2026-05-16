c4c858ef260649b6a7fdc8cd6a1978eb1adc64b6
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2026-05-16 06:24:59 +0100

    Fix build error in seed-employees script by adding dotenv and update dist files

--- server/dist/common/decorators/current-user.decorator.d.ts
+++
@@ -1,7 +0,0 @@
-export interface JwtPayload {
-    sub: string;
-    email: string;
-    role: string;
-    orgId: string;
-}
-export declare const CurrentUser: (...dataOrPipes: unknown[]) => ParameterDecorator;
--- server/dist/users/dto/create-user.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserRole } from '../../common/enums';
-export declare class CreateUserDto {
-    name: string;
-    email: string;
-    password: string;
-    orgName: string;
-    role: UserRole;
-}
